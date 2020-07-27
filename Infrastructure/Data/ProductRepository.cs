a5d3ce846b6484bfb241cf903142f7a79cd50911
JonasOliveira <jonas.alves.oliveira@gmail.com>
2020-07-27 00:40:15 -0300

    Add Helpper, DTO, Mapper, Resolver Url, Images

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -15,14 +15,14 @@ namespace Infrastructure.Data
         }
 
         public async Task<Product> GetProductByIdAsync(int id)
-        {
+        {       
             return await _context.Products
-                        .Include(p => p.ProductType)
+            .Include(p => p.ProductType)
             .Include(p => p.ProductBrand)
             .FirstOrDefaultAsync(p => p.Id == id);
         }
 
-        public async Task<IReadOnlyList<Product>> GetProducsAsync()
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
             return await _context.Products
             .Include(p => p.ProductType)
