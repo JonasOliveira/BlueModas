77810a6430e33773765a2fab85cda89c6dd2f1de
JonasOliveira <jonas.alves.oliveira@gmail.com>
2020-07-28 01:40:27 -0300

    S6 - Adding sorting specification

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -6,13 +6,30 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification()
+        public ProductsWithTypesAndBrandsSpecification(string sort)
         {
             AddInclude(X => X.ProductType);
             AddInclude(X => X.ProductBrand);
+            AddOrderBy(x => x.Name);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "PriceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(p => p.Name);
+                        break;
+                }
+            }
         }
 
-        public ProductsWithTypesAndBrandsSpecification(int id) 
+        public ProductsWithTypesAndBrandsSpecification(int id)
             : base(x => x.Id == id)
         {
             AddInclude(X => X.ProductType);
